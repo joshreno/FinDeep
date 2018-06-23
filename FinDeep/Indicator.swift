a83eaa0f35bd2af9b60cfbb983598f0bfe10247d
Joshua Reno <jreno@Joshuas-MBP.home>
2018-06-23 18:17:38 -0400

    added IndicatorType and Indicator

---
+++ FinDeep/Indicator.swift
@@ -0,0 +1,15 @@
+//
+//  Indicator.swift
+//  FinDeep
+//
+//  Created by Joshua Reno on 6/23/18.
+//  Copyright © 2018 Joshua Reno. All rights reserved.
+//
+
+import Foundation
+
+protocol Indicator {
+    var name : String {get}
+    var type : IndicatorType {get}
+    
+}
