701abfba7fd515472c6ed9d5055df81bf459a9a6
Joshua Reno <joshuagregr@gmail.com>
2018-07-01 12:49:26 -0400

    added methods in Stock

--- FinDeep/Stock.swift
+++ FinDeep/Stock.swift
@@ -19,17 +19,31 @@ enum DataType : String {
     case Dividend_Yield
     case Year_Long_High
     case Year_Long_Low
+    // if market not closed:
+    case PreviousClose
+    case CurrentPrice
 }
 
 class Stock {
     var Stocks: [StockObject]? = nil
-    func getStockData(symbol : String, startDate : Date, endDate : Date, dataTypes : [DataType]) {
-        
+    func getStockData(symbol : String, startDate : Date, endDate : Date, dataTypes : [DataType]) {}
+    func addStock(symbol: String) {
+        var stock: StockObject = StockObject(name: symbol)
     }
 }
 
 class StockObject {
-    var name: String = "Stock"
+    private var name: String = "Stock"
+    private var high: Double = 0.00
+    private var low: Double = 0.00
+    private var open: Double = 0.00
+    private var close: Double = 0.00
+    private var current: Double = 0.00
+    private var previousClose: Double = 0.00
+    
+    init (name: String) {
+        self.name = name
+    }
     
     func getName() -> String {
         return name
@@ -37,5 +51,53 @@ class StockObject {
     
     func setName(name: String) {
         self.name = name
+    }
+    
+    func getHigh() -> Double {
+        return high
+    }
+    
+    func setHigh(high: Double) {
+        self.high = high
+    }
+    
+    func getLow() -> Double {
+        return low
+    }
+    
+    func setLow(low: Double) {
+        self.low = low
+    }
+    
+    func getOpen() -> Double {
+        return open
+    }
+    
+    func setOpen(open: Double) {
+        self.open = open
+    }
+    
+    func getClose() -> Double {
+        return close
+    }
+    
+    func setClose(close: Double) {
+        self.close = close
+    }
+    
+    func getCurrent() -> Double {
+        return current
+    }
+    
+    func setCurrent(current: Double) {
+        self.current = current
+    }
+    
+    func getPreviousClose() -> Double {
+        return previousClose
+    }
+    
+    func setPreviousClose(previousClose: Double) {
+        self.previousClose = previousClose
     }
 }
